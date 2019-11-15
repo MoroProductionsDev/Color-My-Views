bfe8d8ec337c1f7f44e94a34838539d4f3aea96b
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-11-14 19:06:39 -0800

    - added buttons that change the color of box three, four, and five to either red, green or yellow. (Eventhandlers)

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListener() {
         val clickableView: List<View> = listOf(id_textbox_one, id_textbox_two, id_textbox_three,
-                                                id_textbox_four, id_textbox_five, id_constrain_layout)
-
+                                                id_textbox_four, id_textbox_five, id_constrain_layout,
+                    id_red_btn, id_yellow_btn, id_green_btn)
         for (item in clickableView) {
             item.setOnClickListener { makeColored(it) }
         }
@@ -26,7 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view : View) {
         when (view.id) {
-
             // Boxes using Color class colors for background
             R.id.id_textbox_one -> view.setBackgroundColor(Color.DKGRAY)
             R.id.id_textbox_two -> view.setBackgroundColor(Color.GRAY)
@@ -35,6 +35,11 @@ class MainActivity : AppCompatActivity() {
             R.id.id_textbox_three -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.id_textbox_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.id_textbox_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.id_green_btn -> id_textbox_three.setBackgroundResource(R.color.my_green)
+            R.id.id_red_btn -> id_textbox_four.setBackgroundResource(R.color.my_red)
+            R.id.id_yellow_btn -> id_textbox_five.setBackgroundResource(R.color.my_yellow)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
